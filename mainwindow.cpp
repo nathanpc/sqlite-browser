ee1ddb70c85da22133c45afcad7e1f0f3ae1999f
Nathan Campos <eeepc904@gmail.com>
2013-10-28 15:26:32 -0200

    Starting to work with the table population

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,6 +21,23 @@ MainWindow::~MainWindow() {
 	delete ui;
 }
 
+/**
+ * Populates the TableView.
+ *
+ * @param cols Collumn names.
+ * @param rows Rows.
+ */
+void MainWindow::populate_table(vector<QString> cols, vector<vector<QString> > rows) {
+	for (size_t row = 0; row < table.size(); ++row) {
+		for (size_t col = 0; col < cols->size(); ++col) {
+			//rows[row][col]
+		}
+	}
+}
+
+/**
+ * Handles the database opening.
+ */
 void MainWindow::on_actionOpen_Database_triggered() {
 	QString path = QFileDialog::getOpenFileName(this, tr("Open Database"), "",
 												tr("SQLite Databases (*.db *.sqlite);;Files (*.*)"));
@@ -34,18 +51,29 @@ void MainWindow::on_actionOpen_Database_triggered() {
 		alert->setText("Error");
 		alert->setInformativeText(sql.sqlite_error_msg(fail));
 		alert->exec();
+	} else {
+		// Populate TreeView with the tables.
+		vector<vector<QString> > tables = sql.query("SELECT name FROM sqlite_master WHERE type = 'table'", NULL);
+		QStringListModel *model = new QStringListModel();
+
+		for (size_t i = 0; i < tables.size(); ++i) {
+			tables_list.push_back(tables[0][i]);
+		}
+
+		model->setStringList(tables_list);
+		ui->treeView->setModel(model);
 	}
+}
 
-	// Populate TreeView with the tables.
-	vector<vector<QString> > tables = sql.query("SELECT name FROM sqlite_master WHERE type = 'table'", NULL);
+/**
+ * Selects a table to work with.
+ *
+ * @param index TreeView model index.
+ */
+void MainWindow::on_treeView_clicked(const QModelIndex &index) {
+	// Dump the whole table into the TableView.
+	vector<QString> *cols;
+	vector<vector<QString> > table = sql.query("SELECT * FROM " + tables_list[index.row()], cols);
 
-	QStringList tables_list;
-	QStringListModel *model = new QStringListModel();
-
-	for (size_t i = 0; i < tables.size(); ++i) {
-		tables_list.push_back(tables[0][i]);
-	}
-
-	model->setStringList(tables_list);
-	ui->treeView->setModel(model);
+	populate_table(cols, table);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,12 +15,18 @@ class MainWindow : public QMainWindow {
 		explicit MainWindow(QWidget *parent = 0);
 		~MainWindow();
 
+		void populate_table(vector<QString> cols, vector<vector<QString> > rows);
+
 	private slots:
 		void on_actionOpen_Database_triggered();
+
+		void on_treeView_clicked(const QModelIndex &index);
 
 	private:
 		Ui::MainWindow *ui;
 		SQLHelper sql;
+
+		QStringList tables_list;
 };
 
 #endif // MAINWINDOW_H
